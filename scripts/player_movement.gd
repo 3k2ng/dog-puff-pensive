6b213db3179dde97ac81ceebd589d5c2afd3b332
Ansel <anselsulejmani826@gmail.com>
2023-01-04 15:45:53 -0700

    Modified movement and began wokring on animation for the roll

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,43 +1,19 @@
-extends Area2D
+extends KinematicBody2D
 
-var velocity
-var moveSpeed = 100 
-var dodgeSpeed = moveSpeed*5
-# get the size of the viewing port to prevent the character from leaving the screen 
-var screenSize  
+var velocity = Vector2()
+export var move_speed = 100 
 
 func _ready():
-	screenSize = get_viewport_rect().size
-
-
-# in the process function we'll define eight directional movement
-# controls will be wasd and left stick of a controller
-func _process(delta):
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1 
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	# this is fo the dodge roll movement option
-	# checks to see if the player is in motion and has pressed the correct input
-	if velocity.length() > 0 and Input.is_action_just_pressed("dodge_roll"):
-		# if true then velocity calculated with the speed of the dodge instead of normal move speed 
-		velocity = velocity.normalized() * dodgeSpeed
-	else:
-		velocity = velocity.normalized() * moveSpeed
-	#Now that we define movement based on input we want to have the object move 
-	# at the speed previously defined
-	# by using normalized() method you ensure that the speed in 
-	# any direction is the moveSpeed defined
+	pass
 	
-	# need to 
-	# update the position of the object based on velocity and time since last frame
-	position += velocity * delta 
-	position.x = clamp(position.x, 0, screenSize.x)
-	position.y = clamp(position.y, 0, screenSize.y)
 
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("dodge_roll"):
+		
+		$AnimationPlayer.play("dodge_roll")
+		
+	velocity = move_and_slide(Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized() * move_speed)
+	
+	 
 	
