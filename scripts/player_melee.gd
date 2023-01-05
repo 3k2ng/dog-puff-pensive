d7732779fd211fd98bad823fea9ce433a28d6220
Ansel <anselsulejmani826@gmail.com>
2023-01-04 23:19:30 -0700

    Added functionality for the weapon to follow the direction of the mouse, currently there's a bug with the melee attack

---
+++ scripts/player_melee.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+var enemy_attacked
+
+func _ready():
+	self.connect("body_entered", self, "if_body_entered")
+	
+
+
+
+func if_body_entered(body: Node):
+	
+	if body.is_in_group("enemy"):
+		print("enemy is hit")
+		$body.hurt()
+	
+
+func melee_input():
+	if Input.get_connected_joypads().size() > 0:
+		pass
+		# write attack code for controller later 
+	else: 
+		if Input.is_action_just_pressed("melee_attack"):
+			$BoneAnimation.play("bone_melee")
+
+
+
+
+
+
--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -7,13 +7,17 @@ func _ready():
 	pass
 	
 
-func _physics_process(delta):
-	
-	if Input.is_action_pressed("dodge_roll"):
-		
-		$AnimationPlayer.play("dodge_roll")
-		
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("dodge_roll"):
+		$PlayerAnimation.play("dodge_roll")
+	$MeleeArea.melee_input()
 	velocity = move_and_slide(Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized() * move_speed)
 	
-	 
 	
+
+	
+	
+
+
+
+
