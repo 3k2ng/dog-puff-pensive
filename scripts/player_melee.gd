c1e12dd1ece08494b4b9198a1dc438b964bd78ed
Ansel <anselsulejmani826@gmail.com>
2023-01-04 23:50:23 -0700

    Added mouse tracking for the bone weapon

--- scripts/player_melee.gd
+++ scripts/player_melee.gd
@@ -9,22 +9,25 @@ func _ready():
 
 
 func if_body_entered(body: Node):
-	
+	if Input.get_connected_joypads().size() > 0:
+		pass
+		# write attack code for controller later 
+	else: 
+		pass
 	if body.is_in_group("enemy"):
 		print("enemy is hit")
 		$body.hurt()
 	
+func _Input(event):
+	if Input.is_action_just_pressed("melee_attack"):
+		$BoneAnimation.play("")
+
 
 func melee_input():
 	if Input.get_connected_joypads().size() > 0:
 		pass
 		# write attack code for controller later 
 	else: 
+		self.look_at(get_viewport().get_mouse_position())
 		if Input.is_action_just_pressed("melee_attack"):
 			$BoneAnimation.play("bone_melee")
-
-
-
-
-
-
