1c45f80c21870401b98a798c6583c7458824336c
SandersWifeBernie <117328851+SandersWifeBernie@users.noreply.github.com>
2023-01-05 13:44:24 -0700

    Implemented the running animation for horizontal movement, implemented the roll and idle animation as well

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -8,15 +8,24 @@ func _ready():
 	
 
 func _physics_process(_delta):
-	if Input.is_action_just_pressed("dodge_roll"):
-		$PlayerAnimation.play("dodge_roll")
+	
 	$MeleeArea.melee_input()
 	velocity = move_and_slide(Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized() * move_speed)
-	
-	
+	if Input.is_action_just_pressed("dodge_roll"):
+		if velocity.x < 0:
+			$Sprite.flip_h = true
+		else:
+			$Sprite.flip_h = false
+		$PlayerAnimation.play("dodge_roll")
 
-	
-	
+	elif velocity.x > 0 and velocity.y == 0 and $PlayerAnimation.current_animation != "dodge_roll":
+		$Sprite.flip_h = false
+		$PlayerAnimation.play("dog_run")
+	elif velocity.x < 0 and velocity.y == 0 and $PlayerAnimation.current_animation != "dodge_roll":
+		$Sprite.flip_h = true
+		$PlayerAnimation.play("dog_run")
+	elif velocity == Vector2.ZERO and $PlayerAnimation.current_animation != "dodge_roll":
+		$PlayerAnimation.play("idle") 
 
 
 
