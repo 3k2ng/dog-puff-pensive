df3cb0817edae737e9e3da11ea2e96fd9fbb3e60
SADragonMaker <tung09102002@gmail.com>
2023-01-06 05:34:11 -0700

    room following camera

---
+++ scripts/room_camera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+const ROOM_WIDTH: float = 320.0
+const ROOM_HEIGHT: float = 192.0
+
+var target: KinematicBody2D
+var current_room_coord: Vector2 = Vector2.ZERO
+
+func get_player_as_target() -> void:
+	var player_group: Array = get_tree().get_nodes_in_group("player")
+	if len(player_group) > 0:
+		target = get_tree().get_nodes_in_group("player")[0]
+
+func _process(delta: float) -> void:
+	if target:
+		current_room_coord = Vector2(round(target.position.x / ROOM_WIDTH - 0.5), round(target.position.y / ROOM_HEIGHT - 0.5))
+		var target_position = Vector2(current_room_coord.x * ROOM_WIDTH, current_room_coord.y * ROOM_HEIGHT)
+		position += (target_position - position) * delta * 4
+	else:
+		get_player_as_target()
