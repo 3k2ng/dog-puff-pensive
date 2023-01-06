6779910060bb53abb85a79cd04f7fd9f5503ffb0
SADragonMaker <tung09102002@gmail.com>
2023-01-05 23:37:13 -0700

    add controller aim

--- scripts/player_melee.gd
+++ scripts/player_melee.gd
@@ -16,9 +16,8 @@ func if_body_entered(body: Node):
 
 func melee_input():
 	if Input.get_connected_joypads().size() > 0:
-		pass
-		# write attack code for controller later 
+		rotation = Vector2.RIGHT.angle_to(Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down").normalized())
 	else: 
 		self.look_at(get_viewport().get_mouse_position())
-		if Input.is_action_just_pressed("melee_attack"):
-			$BoneAnimation.play("bone_melee")
+	if Input.is_action_just_pressed("melee_attack"):
+		$BoneAnimation.play("bone_melee")
