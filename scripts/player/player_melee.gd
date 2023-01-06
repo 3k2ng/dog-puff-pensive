4a797a26192a999c6a2a95f9cbeaaf996d7f36e7
SADragonMaker <tung09102002@gmail.com>
2023-01-06 12:55:03 -0700

    add more rooms

--- scripts/player/player_melee.gd
+++ scripts/player/player_melee.gd
@@ -22,8 +22,8 @@ func _process(delta: float) -> void:
 	if Input.get_connected_joypads().size() > 0:
 		rotation = Vector2.RIGHT.angle_to(Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down").normalized())
 	else: 
-		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+#		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+#		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		self.look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("melee_attack"):
 		if not $MeleePlayer.is_playing():
