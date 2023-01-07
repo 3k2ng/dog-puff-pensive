8278fe4462ceda42d2b4ae6b2419eca7ab916855
Chloe Nielsen <littleb88888888@gmail.com>
2023-01-07 16:34:38 -0700

    Added pause menu + restart button

---
+++ scripts/management/pause_menu.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+func _ready():
+	visible = false
+
+	var _success = $Restart.connect("button_down", self, "restart")
+	_success = $Quit.connect("button_down", self, "quit")
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+	if event.is_action_pressed("restart"):
+		restart()
+
+func toggle_pause():
+	var tree = get_tree()
+	tree.paused = !tree.paused
+	visible = tree.paused
+
+func restart():
+	get_tree().paused = false
+	var _success = get_tree().change_scene("res://scenes/game.tscn")
+
+func quit():
+	get_tree().quit()
