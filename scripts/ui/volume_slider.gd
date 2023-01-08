60f7ecacac3ee4a016c20b216f9dcfad1eb04246
Chloe Nielsen <littleb88888888@gmail.com>
2023-01-07 21:56:49 -0700

    Made volume slider transfer to game

--- scripts/ui/volume_slider.gd
+++ scripts/ui/volume_slider.gd
@@ -2,12 +2,16 @@ extends Slider
 
 
 export var bus_name: String
+export var set_value_on_ready: bool = false
 var bus_id: int
 
 func _ready():
 	var _success = self.connect("value_changed", self, "set_volume")
 	bus_id = AudioServer.get_bus_index(bus_name)
-	set_volume(value)
+	if set_value_on_ready:
+		set_volume(value)
+	else:
+		value = db2linear(AudioServer.get_bus_volume_db(bus_id))
 
 func set_volume(value: float):
 	AudioServer.set_bus_volume_db(bus_id, linear2db(value))
