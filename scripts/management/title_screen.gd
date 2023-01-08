c98ce8609c8d4dce59014aa5768f08c7283b56ab
Chloe Nielsen <littleb88888888@gmail.com>
2023-01-07 18:05:39 -0700

    Added quit button to title screen

--- scripts/management/title_screen.gd
+++ scripts/management/title_screen.gd
@@ -4,9 +4,13 @@ extends CanvasLayer
 func _ready():
 	$Play.grab_focus()
 	var _success = $Play.connect("button_down", self, "play")
+	_success = $Quit.connect("button_down", self, "quit")
 
 func play():
 	var _success = get_tree().change_scene("res://scenes/game.tscn")
+
+func quit():
+	get_tree().quit()
 
 func _unhandled_input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
