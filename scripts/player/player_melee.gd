f201a056c8df497087f09110f62044824d1cb9a6
Chloe Nielsen <littleb88888888@gmail.com>
2023-01-07 22:44:27 -0700

    Tweaked player melee attack
    -split swings into initial and followup, initial has no delay
    -added melee input buffer

--- scripts/player/player_melee.gd
+++ scripts/player/player_melee.gd
@@ -3,6 +3,9 @@ extends Area2D
 const SWINGING_SOUND: AudioStreamMP3 = preload("res://sfxs/whoosh-6316.mp3")
 const HITTING_SOUND: AudioStreamSample = preload("res://sfxs/06-hitting.wav")
 
+export var input_buffer_time: float = 0.1
+var input_buffer_timer: float
+
 onready var anim_playback = $AnimationTree.get("parameters/playback")
 
 func _ready():
@@ -17,13 +20,24 @@ func _body_entered(body: Node) -> void:
 		play_sound(HITTING_SOUND, true)
 	
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if Input.get_connected_joypads().size() > 0:
 		rotation = Vector2.RIGHT.angle_to(Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down").normalized())
 	else: 
 		self.look_at(get_global_mouse_position())
+
 	if Input.is_action_just_pressed("melee_attack"):
-		anim_playback.travel("swing")
+		# anim_playback.travel("swing")
+		set_swing(true)
+		input_buffer_timer = input_buffer_time
+	else:
+		input_buffer_timer -= delta
+		if(input_buffer_timer <= 0):
+			set_swing(false)
+
+
+func set_swing(value: bool) -> void:
+	$AnimationTree["parameters/conditions/swing"] = value
 
 func _swing_start() -> void:
 	play_sound(SWINGING_SOUND, true)
